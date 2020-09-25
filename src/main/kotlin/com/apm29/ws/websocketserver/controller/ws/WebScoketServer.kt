96d26f22de79845aea4239e481190164171b6841
apm29 <yjw999wow@163.com>
2020-09-25 17:34:56 +0800

    WebRTC Server Test

--- src/main/kotlin/com/apm29/ws/websocketserver/controller/ws/WebScoketServer.kt
+++ src/main/kotlin/com/apm29/ws/websocketserver/controller/ws/WebScoketServer.kt
@@ -1,40 +1,44 @@
 package com.apm29.ws.websocketserver.controller.ws
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.io.ByteArrayInputStream
 import java.io.IOException
+import java.util.HashMap
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
 import javax.websocket.*
 import javax.websocket.server.PathParam
 import javax.websocket.server.ServerEndpoint
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 /**
  * @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
  * 注解的值将被用于监听用户连接的终端访问URL地址,客户端可以通过这个URL来连接到WebSocket服务器端
  */
+
 @ServerEndpoint("/websocket/{userId}")
 @Component
 class WebSocketServer {
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
-    private var WebSocketsession: Session? = null
+    private lateinit var webSocketSession: Session
 
     //当前发消息的人员userId
-    private var userId = ""
+    private lateinit var userId: String
 
     /**
      * 连接建立成功调用的方法 */
     @OnOpen
-    fun onOpen(@PathParam(value = "userId") param: String, WebSocketsession: Session?, config: EndpointConfig?) {
-        userId = param
+    fun onOpen(@PathParam(value = "userId") param: String, WebSocketsession: Session, config: EndpointConfig?) {
+        this.userId = param
         //log.info("authKey:{}",authKey);
-        this.WebSocketsession = WebSocketsession
+        this.webSocketSession = WebSocketsession
         webSocketSet[param] = this //加入map中
         val cnt = OnlineCount.incrementAndGet() // 在线数加1
         logger.info("有连接加入，当前连接数为：{}", cnt)
-        sendMessage(this.WebSocketsession, "连接成功")
     }
 
     /**
@@ -57,7 +61,51 @@ class WebSocketServer {
     @OnMessage
     fun onMessage(message: String, session: Session?) {
         logger.info("来自客户端的消息：{}", message)
-        sendMessage(session, "收到消息，消息内容：$message")
+        val hashMap = objectMapper.readValue(message, Map::class.java)
+        when {
+            hashMap["type"] == REGISTER -> {
+                sendMessage(this.webSocketSession, mapOf(
+                        "type" to REGISTER,
+                        "code" to 1
+                ))
+            }
+            hashMap["type"] == OFFER -> {
+                val sdp = hashMap["sdp"]
+                //转发给其他session
+                webSocketSet.forEach { (userId: String, server: WebSocketServer) ->
+                    if (userId != this.userId)
+                        sendMessage(server.webSocketSession, mapOf(
+                                "type" to OFFER,
+                                "sdp" to sdp,
+                                "code" to 1
+                        ))
+                }
+            }
+            hashMap["type"] == CALL -> {
+                //给其他人发IN_CALL
+                webSocketSet.forEach { (userId: String, server: WebSocketServer) ->
+                    println("current:${this.userId}")
+                    println("inSet:$userId")
+                    if (userId != this.userId)
+                        sendMessage(server.webSocketSession, mapOf(
+                                "type" to IN_CALL,
+                                "code" to 1
+                        ))
+                }
+            }
+            hashMap["type"] == CANDIDATE -> {
+                val candidate = hashMap["candidate"]
+                //给其他人发IN_CALL
+                webSocketSet.forEach { (userId: String, server: WebSocketServer) ->
+                    if (userId != this.userId)
+                        sendMessage(server.webSocketSession, mapOf(
+                                "type" to CANDIDATE,
+                                "candidate" to candidate,
+                                "code" to 1
+                        ))
+                }
+            }
+        }
     }
 
     /**
@@ -77,8 +125,26 @@ class WebSocketServer {
      */
     fun sendMessage(session: Session?, message: String?) {
         try {
-            session!!.basicRemote.sendText(String.format("%s (From Server，Session ID=%s)", message, session.id))
+            session?.basicRemote?.sendText(String.format("%s (From Server，Session ID=%s)", message, session.id))
             //session.getBasicRemote().sendText(String.format("%s",message));
+        } catch (e: IOException) {
+            logger.error("发送消息出错：{}", e.message)
+            e.printStackTrace()
+        }
+    }
+
+    val objectMapper = ObjectMapper()
+
+    /**
+     * 发送消息，实践表明，每次浏览器刷新，session会发生变化。
+     * @param message
+     */
+    fun sendMessage(session: Session?, message: Map<*, *>) {
+        logger.info("发送消息：{}", message.toString())
+        try {
+            session?.basicRemote?.sendText(
+                    objectMapper.writeValueAsString(message)
+            )
         } catch (e: IOException) {
             logger.error("发送消息出错：{}", e.message)
             e.printStackTrace()
@@ -92,7 +158,7 @@ class WebSocketServer {
      */
     fun broadCastInfo(message: String?) {
         for (key in webSocketSet.keys) {
-            val session = webSocketSet[key]!!.WebSocketsession
+            val session = webSocketSet[key]?.webSocketSession
             if (session != null && session.isOpen && userId != key) {
                 sendMessage(session, message)
             }
@@ -106,8 +172,8 @@ class WebSocketServer {
      */
     fun sendToUser(userId: String?, message: String?) {
         val webSocketServer = webSocketSet[userId]
-        if (webSocketServer != null && webSocketServer.WebSocketsession!!.isOpen) {
-            sendMessage(webSocketServer.WebSocketsession, message)
+        if (webSocketServer != null && webSocketServer.webSocketSession.isOpen) {
+            sendMessage(webSocketServer.webSocketSession, message)
         } else {
             logger.warn("当前用户不在线：{}", userId)
         }
@@ -121,5 +187,11 @@ class WebSocketServer {
 
         //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
         private val webSocketSet = ConcurrentHashMap<String, WebSocketServer>()
+
+        const val REGISTER = "register"
+        const val OFFER = "offer"
+        const val CALL = "call"
+        const val IN_CALL = "in_call"
+        const val CANDIDATE = "candidate"
     }
 }
