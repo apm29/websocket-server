9bbc7098b22772083f0a8c62a450b3991ca3cadf
apm29_win10 <yjw999wow@163.com>
2020-10-08 21:49:45 +0800

    feature[jpa]:users

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.3.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm") version "1.3.50"
+    kotlin("plugin.spring") version "1.3.50"
 }
 
 group = "com.apm29.ws"
@@ -12,7 +12,11 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-    mavenCentral()
+    maven ("https://plugins.gradle.org/m2/")
+    maven ("https://maven.aliyun.com/repository/gradle-plugin")
+    maven ("https://maven.aliyun.com/repository/google")
+    maven ("http://maven.aliyun.com/nexus/content/groups/public/")
+    maven ("https://maven.aliyun.com/repository/jcenter")
 }
 
 dependencies {
@@ -22,6 +26,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:6.0.6")
 }
 
 tasks.withType<Test> {
---
+++ src/main/kotlin/com/apm29/ws/websocketserver/db/entity/User.kt
@@ -0,0 +1,5 @@
+package com.apm29.ws.websocketserver.db.entity
+
+class User {
+
+}
